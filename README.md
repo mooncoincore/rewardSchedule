808350df2fd19c833efa8b4f3814d53686016889
Vassilis <bkrith@hotmail.com>
2017-12-11 16:42:50 +0200

    Transfer from old Repo

---
+++ README.md
@@ -0,0 +1,6 @@
+compile with:
+
+ gcc moonreward.c -lm -o moonreward
+
+love,
+baz
---
+++ moonreward.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+int main(){
+ int nHeight, nSubsidy, nSubsidyPrev;
+ for(nHeight=1100000;nHeight<1250000;nHeight++){
+  nSubsidy = floor( 19697202017 / (floor(nHeight/100000)*100000) );
+  if(nSubsidy!=nSubsidyPrev){
+   printf("block: %d reward: %d\n", nHeight, nSubsidy);
+   nSubsidyPrev=nSubsidy;
+  }
+ }
+ for(nHeight=1250000;nHeight<5000000;nHeight++){
+  nSubsidy = floor( floor(0.29531*19697202017) / (floor(nHeight/100000)*100000) );
+  if(nSubsidy!=nSubsidyPrev){
+   printf("block: %d reward: %d\n", nHeight, nSubsidy);
+   nSubsidyPrev=nSubsidy;
+  }
+ }
+}
+
+
